d04fb58c5b9c850eac32d0b3351c2fab85df70ae
RosmeoP <133540733+RosmeoP@users.noreply.github.com>
2024-11-17 18:16:30 -0600

    practica

---
+++ DataAccessLayer/DbConnection/SqlDataAccess.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DbConnection
+{
+	public class SqlDataAccess
+	{
+		private readonly string _connectionString;
+
+		public SqlDataAccess()
+		{
+			_connectionString = "Data Source=DESKTOP-J4U66GV\\SQLEXPRESS;Initial Catalog=StudentAdministratorDB;Integrated Security=True;Encrypt=False;Trust Server Certificate=True";
+		}
+
+		public SqlConnection GetConnection()
+		{
+			return new SqlConnection(_connectionString);
+		}
+	}
+}
